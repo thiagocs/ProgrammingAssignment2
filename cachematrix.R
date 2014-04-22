50b11c3955c8401053ee5877f83806644f2561ea
Roger D. Peng [amelia] <rdpeng@gmail.com>
2014-04-22 10:09:22 -0400

    More typos

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+
+## The function below creates a "matrix" object which caches its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        
+        xinv <- NULL 
+        
+        set <- function(y) {
+                x <<- y
+                xinv <<- NULL 
+        }
+        
+        get <- function() x 
+        setInv <- function(inv) xinv <<- inv 
+        getInv <- function() xinv 
+        list(set = set, get = get,
+             setInv = setInv,
+             getInv = getInv)
 }
 
 
-## Write a short comment describing this function
+## The function belown computes the inverse of the special "matrix" returned by the function above. If the inverse is already done
+## cacheSolve will return its previous result from cache therefore saving computational efforts.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getInv() 
+        if(!is.null(m)) { 
+                
+                return(m) 
+        }
+        data <- x$get() 
+        m <- solve(data) 
+        x$setInv(m) 
+        m 
 }
